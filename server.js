00a7d9f84e951e3171395113ba36e6a438bbd3d1
Jürgen Key <jkey@arcor.de>
2019-12-14 19:52:39 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 7070;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const ELK = require('elkjs')
+const elk = new ELK()
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  elk.layout(req.body)
+   .then(function(g) {
+     res.json(g);//res.send(JSON.stringify(g, null, " "));
+   })
+   .catch(function(g) {
+     res.status(500).send(g)
+   })
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+
+
+
