2d91fdcb82bd2610bd39899ad13b83e75a368ef1
Jürgen Key <jkey@arcor.de>
2020-05-02 08:50:20 +0200

    added possibility to send graph as multipart form data

--- server.js
+++ server.js
@@ -17,14 +17,48 @@ const ELK = require('elkjs')
 const elk = new ELK()
 
 app.post('/', (req, res) => {
-  console.log(req.body);
-  elk.layout(req.body)
-   .then(function(g) {
-     res.json(g);//res.send(JSON.stringify(g, null, " "));
-   })
-   .catch(function(g) {
-     res.status(500).send(g)
-   })
+  if (req.is('multipart/form-data'))
+  {
+    console.log('multipart');
+    console.log(req.param.jsonGraph);
+    elk.layout(req.param.jsonGraph)
+     .then(function(g) {
+       res.json(g);//res.send(JSON.stringify(g, null, " "));
+    })
+      .catch(function(g) {
+      res.status(500).send(g)
+    })
+  }
+  else if (req.is('application/x-www-form-urlencoded'))
+  {
+    console.log('urlencoded');
+    console.log(req.body.jsonGraph);
+    elk.layout(req.body.jsonGraph)
+     .then(function(g) {
+       res.json(g);//res.send(JSON.stringify(g, null, " "));
+    })
+      .catch(function(g) {
+      res.status(500).send(g)
+    })
+  }
+  else
+  {
+    if (!(req.is('application/json')))
+    {
+      res.status(500).send(g)
+    }
+    else
+    {
+      console.log(req.body);
+      elk.layout(req.body)
+       .then(function(g) {
+         res.json(g);//res.send(JSON.stringify(g, null, " "));
+       })
+       .catch(function(g) {
+         res.status(500).send(g)
+       })
+    }
+  }
 });
 
 app.listen(PORT, HOST);
