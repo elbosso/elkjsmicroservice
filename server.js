8d2d19521d57a946c8e0e4ce9f3c3c0e7e012a85
Jürgen Key <jkey@arcor.de>
2020-05-02 08:06:19 +0200

    add page for testing

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'));
 
 const ELK = require('elkjs')
 const elk = new ELK()
